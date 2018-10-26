5193b3d72cd4e56cfb2c92e5868b41ed11e57bb9
Marzena Malachowska <malachowska.marzena@gmail.com>
2018-10-26 21:27:50 +0200

    Move single project to ProjectSummary.js file

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProjectSummary = () => (
+  <div className="card z-depth-0 project-summary">
+    <div className="card-content grey-text text-darken-3">
+      <span className="card-title">Project Title</span>
+      <p>Posted by the PolishCodeGirl</p>
+      <p className="grey-text">26th October, 9pm</p>
+    </div>
+  </div>
+);
+
+export default ProjectSummary;
